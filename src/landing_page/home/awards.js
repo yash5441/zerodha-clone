48df83d139e9b9fdf444355f9bcbf40875efa630
Yash5441 <22BCS12976@cuchd.in>
2025-06-25 11:45:52 +0530

    added pictures to hero page

--- src/landing_page/home/awards.js
+++ src/landing_page/home/awards.js
@@ -1,7 +1,34 @@
 import React from 'react';
+
 function Awards() {
     return (  
-       <h1>Awards</h1>
+       <div className='container p-5 mb-5'>
+           <div className='row'>
+               <div className='col-6'>
+                   <img src='/images/largestbroker.svg' alt='largest broker' />
+               </div>
+               <div className='col-6'>
+                    <h1>Largest Stock Broker in India</h1>
+                    <p>2 million Zerodha clients contribute to over 15% of all volumes in India daily by trading and investing in: </p>
+                    <div className='row'>  
+                        <div className='col-6'>
+                            <ul>
+                                <li><p>Futures and options</p></li>
+                                <li><p>Commodity derivatives</p></li>
+                                <li><p>Currency derivatives</p></li>
+                            </ul>
+                        </div>
+                        <div className='col-6'>
+                            <ul>
+                                <li><p>Futures and options</p></li>
+                                <li><p>Commodity derivatives</p></li>
+                                <li><p>Currency derivatives</p></li>
+                            </ul>
+                        </div>
+                    </div>
+               </div>
+           </div>
+       </div>
     );
 }
 
--- src/landing_page/home/hero.js
+++ src/landing_page/home/hero.js
@@ -1,7 +1,14 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container'>
+            <div className='row text-center'>
+                <img src="/images/coin.png"  alt="hero image" />
+                <h1>INVEST IN EVERYTHING</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds etc. </p>
+                <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"20%" , margin:"0 auto"}}>Signup Now</button>
+            </div>
+        </div>
      );
 }
 
