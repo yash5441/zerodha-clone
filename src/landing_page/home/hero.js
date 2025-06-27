92d5f4080715594a1d5c9a05acab8df82231b8d0
Yash5441 <22BCS12976@cuchd.in>
2025-06-27 11:31:40 +0530

    hero changes

--- src/landing_page/home/hero.js
+++ src/landing_page/home/hero.js
@@ -3,7 +3,7 @@ function Hero() {
     return ( 
         <div className='container'>
             <div className='row text-center'>
-                <img src="/images/coin.png"  alt="hero image" />
+                <img src="/images/homeHero.png"  alt="hero image" />
                 <h1>INVEST IN EVERYTHING</h1>
                 <p>Online platform to invest in stocks, derivatives, mutual funds etc. </p>
                 <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"20%" , margin:"0 auto"}}>Signup Now</button>
