8c6b4052f8a7f2262e4328c1f64a68130f1fd3a8
Yash5441 <22BCS12976@cuchd.in>
2025-06-27 11:24:22 +0530

    home page

--- src/landing_page/home/awards.js
+++ src/landing_page/home/awards.js
@@ -4,12 +4,12 @@ function Awards() {
     return (  
        <div className='container p-5 mb-5'>
            <div className='row'>
-               <div className='col-6'>
+               <div className='col-6 p-5'>
                    <img src='/images/largestbroker.svg' alt='largest broker' />
                </div>
-               <div className='col-6'>
+               <div className='col-6 p-5'>
                     <h1>Largest Stock Broker in India</h1>
-                    <p>2 million Zerodha clients contribute to over 15% of all volumes in India daily by trading and investing in: </p>
+                    <p className='mb-5'>2 million Zerodha clients contribute to over 15% of all volumes in India daily by trading and investing in: </p>
                     <div className='row'>  
                         <div className='col-6'>
                             <ul>
@@ -20,11 +20,12 @@ function Awards() {
                         </div>
                         <div className='col-6'>
                             <ul>
-                                <li><p>Futures and options</p></li>
-                                <li><p>Commodity derivatives</p></li>
-                                <li><p>Currency derivatives</p></li>
+                                <li><p>Stocks and ipos</p></li>
+                                <li><p>Mutual funds</p></li>
+                                <li><p>Bonds and government securities</p></li>
                             </ul>
                         </div>
+                        <img src='/images/presslogos.png'style={{ width:"90%"}} />
                     </div>
                </div>
            </div>
--- src/landing_page/home/stats.js
+++ src/landing_page/home/stats.js
@@ -1,7 +1,24 @@
 import React from 'react';
 function Stats() {
     return ( 
-        <h1>stats</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6'>'
+                    <h1>Trust with confidence</h1>
+                    <h2>Customer-first always</h2>
+                    <P>That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</P>
+                    <h2>No spam or gimmicks</h2>
+                    <P>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.</P>
+                    <h2>The Zerodha universe</h2>
+                    <P>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</P>
+                    <h2>Do better with money</h2>
+                    <P>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</P>
+                </div>
+                <div className='col-6'>
+                    <img src='/images/ecosystem.png' style={{width:"70%"}} />
+                </div>
+            </div>   
+        </div>
      );
 }
 
