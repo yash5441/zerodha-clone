1e48d285a4837a22bb0a2c10dcf8b2823b8e9e6a
Yash5441 <22BCS12976@cuchd.in>
2025-07-05 19:26:19 +0530

    added file supportpage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {browserRouter, Routes, Route, BrowserRouter} from "react-router-dom"
 import './index.css';
 import Homepage from './landing_page/home/Homepage';
+import Signup from './landing_page/signup/Signup';
+import Aboutpage from './landing_page/about/Aboutpage';
+import Productpage from './landing_page/products/Hero';
+import Pricing from './landing_page/pricing/Pricingpage';
+import Supportpage from './landing_page/support/';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <Homepage />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/about" element={<Aboutpage />}></Route>
+      <Route path="/product" element={<Productpage />}></Route>
+      <Route path="/Pricing" element={<Pricing />}></Route>
+      <Route path="/support" element={<Supportpage />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
--- src/landing_page/home/hero.js
+++ src/landing_page/home/hero.js
@@ -3,10 +3,10 @@ function Hero() {
     return ( 
         <div className='container mt-5 '>
             <div className='row text-center'>
-                <img src="/images/homeHero.png" className="mx-auto d-block" style={{width: "80%"}} alt="hero image" />
-                <h1 className='pt-5'>INVEST IN EVERYTHING</h1>
+                <img src="/images/homeHero.png" className="mx-auto d-block" style={{width: "64%"}} alt="hero image" />
+                <h1 className='pt-5 mb-2 large-heading'>Invest in everything</h1>
                 <p >Online platform to invest in stocks, derivatives, mutual funds etc. </p>
-                <button className='p-2 btn btn-primary fs-4 mb-3' style={{width:"15%" , margin:"0 auto"}}>Signup Now</button>
+                <button className='p-2 btn btn-primary mt-3 fs-5 mb-3' style={{width:"16%" , margin:"0 auto"}}>Signup for free</button>
             </div>
         </div>
      );
