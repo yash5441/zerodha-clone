689bad51f1e1d64023e75942485939862e2eb020
Yash5441 <22BCS12976@cuchd.in>
2025-07-12 22:38:00 +0530

    added routes to all the menus

--- src/index.js
+++ src/index.js
@@ -7,17 +7,20 @@ import Signup from './landing_page/signup/Signup';
 import Aboutpage from './landing_page/about/Aboutpage';
 import Productpage from './landing_page/products/Hero';
 import Pricing from './landing_page/pricing/Pricingpage';
-import Supportpage from './landing_page/support/';
+import Supportpage from './landing_page/support/Supportpage';
+import Topnav from './landing_page/Topnav';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <Topnav />
     <Routes>
       <Route path="/" element={<Homepage />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
       <Route path="/about" element={<Aboutpage />}></Route>
       <Route path="/product" element={<Productpage />}></Route>
       <Route path="/Pricing" element={<Pricing />}></Route>
-      <Route path="/support" element={<Supportpage />}></Route>
+      <Route path="/Supportpage" element={<Supportpage />}></Route>
     </Routes>
+    <Footer />
   </BrowserRouter>
 );
--- src/landing_page/pricing/pricingpage.js
+++ src/landing_page/pricing/pricingpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Topnav from '../Topnav';
-import Hero from './hero';
-import Brokerage from './brokerage';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
 import Openaccount from '../Openaccount';
 import Footer from '../Footer';
 function Pricing() {
