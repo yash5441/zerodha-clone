16827d7473f73c8b29703d625f26555ecc309c28
Yash5441 <22BCS12976@cuchd.in>
2025-07-19 15:09:04 +0530

    ffed

--- src/landing_page/notfound.js
+++ src/landing_page/notfound.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 function NotFound() {
     return ( 
         <div className='container p-3'>
             <div className='row text-center'>
                 <h1>404 not found</h1>
-                <p>sORRY, the page that you are looking for does not exist. </p>
-                <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"20%" , margin:"0 auto"}}>GO HOME</button>
+                <p>SORRY, the page that you are looking for does not exist. </p>
+                <Link to="/"><button className='p-3 btn btn-primary fs-3 mb-1' style={{width:"10%" , margin:"0 auto"}}>GO HOME</button></Link>
             </div>
         </div>
      );
