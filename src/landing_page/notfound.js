e0231ebc64367361d182ebf841f06a643eafc385
Yash5441 <22BCS12976@cuchd.in>
2025-07-19 15:11:09 +0530

    daw

--- src/landing_page/notfound.js
+++ src/landing_page/notfound.js
@@ -6,7 +6,7 @@ function NotFound() {
             <div className='row text-center'>
                 <h1>404 not found</h1>
                 <p>SORRY, the page that you are looking for does not exist. </p>
-                <Link to="/"><button className='p-3 btn btn-primary fs-3 mb-1' style={{width:"10%" , margin:"0 auto"}}>GO HOME</button></Link>
+                <Link to="/"><button className='p-2 btn btn-primary fs-4 mb-3' style={{width:"10%" , margin:"0 auto"}}>GO HOME</button></Link>
             </div>
         </div>
      );
